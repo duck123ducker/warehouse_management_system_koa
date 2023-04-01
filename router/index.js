3df7750ad4190ab86c31cf502c4e10d85bfb6dcd
你çduck123ducker <tencentmail@duck.com>
2023-04-01 18:41:09 +0800

    update

--- router/index.js
+++ router/index.js
@@ -3,8 +3,10 @@ const router = new Router();
 const pack = require('./pack')
 const update = require('./update')
 const create = require('./create')
+const modify = require('./modify')
 
 router.use('/api/pack', pack.routes(), pack.allowedMethods());
 router.use('/api/update', update.routes(), update.allowedMethods());
 router.use('/api/create', create.routes(), create.allowedMethods());
+router.use('/api/modify', modify.routes(), modify.allowedMethods());
 module.exports = router;
